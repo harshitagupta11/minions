846747851f8c06072c00dea017481fba1a6193dc
Harshita <>
2021-04-09 11:08:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.getElementById("btn-translate");
+var inputTxt = document.querySelector("#input-textarea");
+var outputDiv = document.querySelector("#output-text");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+btnTranslate.addEventListener("click",function clickHandler(){
+    var inputText = inputTxt.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+});
